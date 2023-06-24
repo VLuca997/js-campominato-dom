e246068f62fe9e13ded246f4c7661dcf4e6f09d0
VLuca997 <kulertoast997@gmail.com>
2023-06-24 02:06:01 +0200

    ADD element. ADD human Code, ADD HTML

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+    1) inizializzo le variabili per : N° bombe, punteggio.
+    2) Genero casualmente il posizionaemento delle bombe
+    3) aggiungo una gestione di eventi click per gli elementi della griglia
+        - verifico se l'elemento cliccato è una bomba
+            -SE è una bomba: il gioco finisce.
+            -ALTRIMENTI: il gioco prosegue, mi stampa un punto.
+        -verifichiamo se l'utente ha cliccato tutte le celel non bombe
+    4)  al termine della partita, stampa il risultato finale.
+*/
+
+
+
